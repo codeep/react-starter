24912bdbb03c10e2b97180ebb4a414d7fdfbadbc
Artyomaaa <artyom.aramyan@mail.ru>
2018-08-28 20:49:23 +0400

    React Router init

---
+++ src/components/Menu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Menu = () => (
+    <div>
+        <ol>
+            <li>Item1</li>
+            <li>Item2</li>
+            <li>Item3</li>
+            <li>Item4</li>
+        </ol>
+    </div>
+);
+
+export default Menu;
